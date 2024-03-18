8e5ceec9245df32ceb8a33722f7efac31e1effa7
Coder-yeah <xiaoyeqiu@hotmail.com>
2024-03-18 11:15:14 +0800

    change to resnet18

--- nets/siamese.py
+++ nets/siamese.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn import Identity
 
 from nets.vgg import VGG16
-from resnet import Resnet50
+from nets.resnet import Resnet18
 
 
 # 每次通过一个尺寸为2，stride为2的池化层才会导致图片的输入尺寸发生变化
@@ -42,26 +42,26 @@ class SENet(nn.Module):
 
 
 class Siamese(nn.Module):
-    def __init__(self, input_shape, pretrained=False):          # vgg
-    # def __init__(self, pretrained=False):     # resnet
+    # def __init__(self, input_shape, pretrained=False):          # vgg
+    def __init__(self, pretrained=False):     # resnet
         super(Siamese, self).__init__()
         # resnet:
-        # self.resnet = Resnet50(pretrained=pretrained)
-        # self.resnet.avgpool = Identity()
-        # self.resnet.fc = Identity()
+        self.resnet = Resnet18(pretrained=pretrained)
+        self.resnet.avgpool = Identity()
+        self.resnet.fc = Identity()
         # resnet:
-        # flat_shape = 32768
+        flat_shape = 25088
 
         # vgg16:
-        self.vgg = VGG16(pretrained, 3)  # 主干网络vgg16，已经加载好预训练权重
-        del self.vgg.avgpool
-        del self.vgg.classifier
+        # self.vgg = VGG16(pretrained, 3)  # 主干网络vgg16，已经加载好预训练权重
+        # del self.vgg.avgpool
+        # del self.vgg.classifier
         # 最后使用的卷积核数量是512
         # vgg16卷积结束的尺寸:
         # flat_shape = 512 * get_img_output_length(input_shape[1], input_shape[0])
-        # vgg16
-        # self.fully_connect1 = torch.nn.Linear(flat_shape, 512)
-        # self.fully_connect2 = torch.nn.Linear(512, 1)
+        # vgg16/resnet18:
+        self.fully_connect1 = torch.nn.Linear(flat_shape, 512)
+        self.fully_connect2 = torch.nn.Linear(512, 1)
 
         # 增加通道注意力机制:主要是变换通道数
         # self.vgg = VGG16(pretrained, 3)
@@ -71,8 +71,8 @@ class Siamese(nn.Module):
         # self.vgg.features[6] = SENet(128)
 
         # vgg+余弦距离
-        self.fully_connect1 = torch.nn.Linear(512, 256)
-        self.fully_connect2 = torch.nn.Linear(256, 1)
+        # self.fully_connect1 = torch.nn.Linear(512, 256)
+        # self.fully_connect2 = torch.nn.Linear(256, 1)
 
     def forward(self, x):
         x1, x2 = x
@@ -80,24 +80,24 @@ class Siamese(nn.Module):
         #   我们将两个输入传入到主干特征提取网络
         # ------------------------------------------#
         # vgg16: 3*3*512
-        x1 = self.vgg.features(x1)              # 尺寸为什么会是2*512*3*3的？--->dataset代码
-        x2 = self.vgg.features(x2)
+        # x1 = self.vgg.features(x1)              # 尺寸为什么会是2*512*3*3的？--->dataset代码
+        # x2 = self.vgg.features(x2)
 
         # resnet:
-        # x1 = self.resnet(x1)
-        # x2 = self.resnet(x2)
+        x1 = self.resnet(x1)
+        x2 = self.resnet(x2)
 
         # -------------------------#
         #   横向展平，相减取绝对值，取l1距离
         # -------------------------#
-        # x1 = torch.flatten(x1, 1)
-        # x2 = torch.flatten(x2, 1)
-        # x = torch.abs(x1 - x2)
+        x1 = torch.flatten(x1, 1)
+        x2 = torch.flatten(x2, 1)
+        x = torch.abs(x1 - x2)
 
         # 计算dim=1的余弦相似度，进行连接
-        x1 = x1.view(8, 512, 9)                         # why is 8 = 2*4（batchsize的两倍）？？
-        x2 = x2.view(8, 512, 9)
-        x = torch.cosine_similarity(x1, x2, dim=2)          # 返回每一个特征图的相似度度量，512维的向量，不用展平了
+        # x1 = x1.view(64, 512, 9)                         # why is 8 = 2*4（batchsize的两倍）？？
+        # x2 = x2.view(64, 512, 9)
+        # x = torch.cosine_similarity(x1, x2, dim=2)          # 返回每一个特征图的相似度度量，512维的向量，不用展平了
 
         # -------------------------#
         #   进行两次全连接，变尺寸进行余弦相似度计算？
@@ -111,5 +111,5 @@ if __name__ == '__main__':
     input = torch.ones((2, 3, 224, 224))
     print(input.shape)
     # 创建实例，没有传播
-    m = Siamese(input_shape=input.shape)
+    m = Siamese()
     print(m)
